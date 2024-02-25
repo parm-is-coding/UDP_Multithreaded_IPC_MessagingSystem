c2ca7f0599cdcddc54d8685fda2f629a3a55d42e
parm <parm@ubuntu20.myguest.virtualbox.org>
2024-02-25 14:09:03 -0800

    add list to receiver

--- receiver.c
+++ receiver.c
@@ -3,16 +3,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "list.h"
 #include "receiver.h"
+
+#define BUFFERSIZE 100
 //static pthread_t thread;
 static char* rxPort;
+static char* msgToPrint;
+static List* pRecieverToPrinterBuffer;
+
+
+//initialize Receiver static variables and run thread fn
+//since local port is data shared between processes it is a critical section
+
+//void receiverThread(){
+// 
+//
+//}
+
+
+
+
+//readBuffer() 
+//stores item at the front of the list inside passed char*
+//list.removeFront()
+//returns the item at the front of the list  
+void printerThread(){
+    while(pRecieverToPrinterBuffer->numElements == 0);
+    //entry_sec()
+    {//Critical Section 
+        //readBuffer();
+    }
+
+}
+
+
 void Receiver_init(const char* localPort){
+    pRecieverToPrinterBuffer = List_create();
     rxPort = (char*)malloc(100);
+    msgToPrint = (char*)malloc(500);
     strncpy(rxPort,localPort,100);
     printf("Recieving From Port#%s\n",rxPort);
+
 
 }
 void Receiver_shutDown(void){
     //pthread_join(&thread,NULL);
     free(rxPort);
+    free(msgToPrint);
 }
