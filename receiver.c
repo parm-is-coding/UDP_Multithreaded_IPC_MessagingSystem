4342802528ba1c53feca9c7f9c03162aef0aed01
parm <parm@ubuntu20.myguest.virtualbox.org>
2024-02-24 21:55:07 -0800

    receiver_init stores localPortnumber dynamically

--- receiver.c
+++ receiver.c
@@ -1,9 +1,17 @@
 #include <pthread.h>
+#include <string.h>//strncpy
 #include "receiver.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+//static pthread_t thread;
+static char* rxPort;
 void Receiver_init(const char* localPort){
-
+    rxPort = (char*)malloc(100);
+    strncpy(rxPort,localPort,100);
+    printf("\n%s",rxPort);
 }
 void Receiver_shutDown(void){
-    
+    //pthread_join(&thread,NULL);
+    free(rxPort);
 }
