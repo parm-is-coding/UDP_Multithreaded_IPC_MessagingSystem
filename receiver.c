6fa46270ca4dc5dc96c22cb33aae6a14427dab3c
parm <parm@ubuntu20.myguest.virtualbox.org>
2024-02-24 21:57:28 -0800

    add print statement for testing

--- receiver.c
+++ receiver.c
@@ -1,15 +1,16 @@
 #include <pthread.h>
 #include <string.h>//strncpy
-#include "receiver.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "receiver.h"
 //static pthread_t thread;
 static char* rxPort;
 void Receiver_init(const char* localPort){
     rxPort = (char*)malloc(100);
     strncpy(rxPort,localPort,100);
-    printf("\n%s",rxPort);
+    printf("\nRecieving From Port#%s",rxPort);
+
 }
 void Receiver_shutDown(void){
     //pthread_join(&thread,NULL);
