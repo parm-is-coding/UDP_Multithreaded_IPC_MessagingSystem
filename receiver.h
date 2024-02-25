011a781dd949ef0d209958f701856258a752d471
parm <parm@ubuntu20.myguest.virtualbox.org>
2024-02-25 14:58:08 -0800

    update receiver_init to take mutex variable

--- receiver.h
+++ receiver.h
@@ -1,7 +1,7 @@
 #ifndef _RECEIVER_H
 #define _RECEIVER_H
 
-void Receiver_init(const char* localPort);
+void Receiver_init(const char* localPort,pthread_mutex_t* plistAddOrRemoveMutex);
 void Receiver_shutDown(void);
 
 
