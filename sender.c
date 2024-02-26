dcfb7c5879e8509b8c9ff0c8eb0edbee7c7d86b7
parm <parm@ubuntu20.myguest.virtualbox.org>
2024-02-26 15:57:06 -0800

    sender somewhat works

--- sender.c
+++ sender.c
@@ -4,34 +4,54 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
 
 #include "sender.h"
 #include "list.h"
 static char* senderIp;
 static int senderPort;
-
+static char* messageTx;
 static pthread_t threadSenderID;
+static int socketFD;
+static struct sockaddr_in serverAddr;
+static int sin_len;
 //static pthread_t threadKeyboardID; 
 //static List* pKeyboardToSenderBuffer;
 //static pthread_mutex_t keyboardToSenderMutex = PTHREAD_MUTEX_INITIALIZER
 
 void* senderThread(void* unused){
-    
-    printf("Send thread is active");
+    printf("Send thread is active\n");
     while(1){
-
+        printf("Enter a message: ");
+        fgets(messageTx, 100, stdin); // Read a line from the console
+        sendto(socketFD,messageTx,strlen(messageTx),
+        0,(struct sockaddr*) &serverAddr,sin_len);
+    
     }
 }
 
 void Sender_init(const char* remIp,const char* remPort){
+    //initialize variables
+    messageTx = malloc(100);
     senderIp = (char*)malloc(100);
     senderPort = atoi(remPort);
     strncpy(senderIp,remIp,100);
+    socketFD = socket(AF_INET,SOCK_DGRAM,0); //init socket
+    memset(&serverAddr, 0, sizeof(serverAddr)); //init serverAddr
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(senderPort);
+    serverAddr.sin_addr.s_addr = inet_addr(senderIp);
+    sin_len = sizeof(serverAddr);
+    bind(socketFD,(struct sockaddr*) &serverAddr,sin_len);
+ 
     printf("Sending To Address %s Port# %d\n",senderIp,senderPort);
     pthread_create(&threadSenderID,NULL,senderThread,NULL);
 }
 void Sender_shutDown(void){
     free(senderIp);
+    free(messageTx);
     pthread_join(threadSenderID,NULL);
-    
+    close(socketFD);   
 }
